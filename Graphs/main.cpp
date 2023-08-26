69a36ef02484d0244ccedced65f8709db64b1a45
cluzze <dm.clozze@gmail.com>
2023-08-27 02:05:27 +0300

    changed input format and added input validation

--- Graphs/main.cpp
+++ Graphs/main.cpp
@@ -1,13 +1,36 @@
 #include "graph.hpp"
 
+#include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace containers;
 
-int main() {
+std::vector<std::pair<int, int>> readInput() {
 	std::vector<std::pair<int, int>> edges;
-	int p, q;
-	while (std::cin >> p >> q) {
-		edges.push_back({p, q});
+	std::string input;
+
+	while (std::getline(std::cin, input)) {
+		std::stringstream ss(input);
+
+		int p, q, w;
+		std::string d1, d2;
+		
+		ss >> p >> d1 >> q >> d2 >> w;
+
+		if (d1 == "--" && d2 == "," && p > 0 && q > 0) {
+			edges.push_back({p, q});
+		}
+		else {
+			std::cerr << "Invalid input" << '\n';
+		}
 	}
+
+	return edges;
+}
+
+int main() {
+	std::vector<std::pair<int, int>> edges = readInput();
 
 	Graph g(edges);
 
