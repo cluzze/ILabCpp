1a46a9457cd4478a0760750e42484e5e09fe72d9
cluzze <dm.clozze@gmail.com>
2023-08-26 20:06:28 +0300

    added big test

--- Graphs/tests.cpp
+++ Graphs/tests.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <cstdlib>
+#include <random>
 
 #include "graph.hpp"
 
@@ -22,8 +23,9 @@ bool dfs_impl(int i, int color, std::vector<int>& used, vector<vector<int>>& gra
     return true;
 }
 
-bool dfs(int v, vector<vector<int>>& graph) {
+bool dfs(vector<vector<int>>& graph) {
     int color = 0;
+    int v = graph.size();
     std::vector<int> used(v, -1);
     
     for (int i = 0; i < v; i++)
@@ -63,7 +65,7 @@ TEST(graph, hand_written1) {
 
     bool res1 = false, res2 = false, res3 = false;
 
-    res1 = dfs(9, graph);
+    res1 = dfs(graph);
     res2 = mygraph.dfs();
     res3 = mygraph.bfs();
 
@@ -82,7 +84,7 @@ TEST(graph, hand_written2) {
     edges.push_back({4, 9});
     edges.push_back({5, 9});
 
-    vector<vector<int>> graph(100);
+    vector<vector<int>> graph(9);
 
     for (int i = 0; i < edges.size(); i++) {
         graph[edges[i].first - 1].push_back(edges[i].second - 1);
@@ -93,7 +95,7 @@ TEST(graph, hand_written2) {
 
     bool res1 = false, res2 = false, res3 = false;
 
-    res1 = dfs(9, graph);
+    res1 = dfs(graph);
     res2 = mygraph.dfs();
     res3 = mygraph.bfs();
 
@@ -107,7 +109,7 @@ TEST(graph, hand_written3) {
     edges.push_back({2, 3});
     edges.push_back({1, 3});
 
-    vector<vector<int>> graph(100);
+    vector<vector<int>> graph(3);
 
     for (int i = 0; i < edges.size(); i++) {
         graph[edges[i].first - 1].push_back(edges[i].second - 1);
@@ -118,7 +120,7 @@ TEST(graph, hand_written3) {
 
     bool res1 = false, res2 = false, res3 = false;
 
-    res1 = dfs(9, graph);
+    res1 = dfs(graph);
     res2 = mygraph.dfs();
     res3 = mygraph.bfs();
 
@@ -164,7 +166,7 @@ TEST(graph, generated1) {
 
     bool res1 = false, res2 = false, res3 = false;
 
-    res1 = dfs(size * 2, graph);
+    res1 = dfs(graph);
     res2 = mygraph.dfs();
     res3 = mygraph.bfs();
 
@@ -211,10 +213,49 @@ TEST(graph, generated2) {
 
     bool res1 = false, res2 = false, res3 = false;
 
-    res1 = dfs(size * 2, graph);
+    res1 = dfs(graph);
     res2 = mygraph.dfs();
     res3 = mygraph.bfs();
 
     ASSERT_EQ(res1, res2);
     ASSERT_EQ(res1, res3);
+}
+
+std::vector<std::pair<int, int>> generateBigBipartiteGraph(int numNodes1, int numNodes2, int numEdges) {
+    std::vector<std::pair<int, int>> graph;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist1(1, numNodes1);
+    std::uniform_int_distribution<int> dist2(numNodes1 + 1, numNodes1 + numNodes2);
+
+    for (int i = 0; i < numEdges; i++) {
+        int node1 = dist1(gen);
+        int node2 = dist2(gen);
+        graph.push_back({node1, node2});
+    }
+
+    return graph;
+}
+
+TEST(graph, generated3) {
+    int numNodes1 = 100000;
+    int numNodes2 = 15000;
+    int numEdges = 500000;
+
+    std::vector<std::pair<int, int>> bipartiteGraph = generateBigBipartiteGraph(numNodes1, numNodes2, numEdges);
+
+    containers::Graph mygraph(bipartiteGraph);
+
+    bool res1 = false, res2 = false, res3 = false;
+
+    vector<vector<int>> graph(numNodes1 + numNodes2);
+
+    for (int i = 0; i < bipartiteGraph.size(); i++) {
+        graph[bipartiteGraph[i].first - 1].push_back(bipartiteGraph[i].second - 1);
+        graph[bipartiteGraph[i].second - 1].push_back(bipartiteGraph[i].first - 1);
+    }
+
+    res1 = dfs(graph);
+    res2 = mygraph.dfs();
+    res3 = mygraph.bfs();
 }
