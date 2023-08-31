70384f90436c4a27243ad957d763e372bde07ae0
cluzze <dm.clozze@gmail.com>
2023-08-31 23:50:44 +0300

    implemented insert

--- Trees/tree.hpp
+++ Trees/tree.hpp
@@ -7,8 +7,14 @@ namespace containers
     template <typename KeyT, typename Comp = std::less<KeyT>>
 	class SearchTree {
     private:
+        enum class Color {
+            Black = 0,
+            Red = 1
+        };
+
         struct TreeNode {
             KeyT key;
+            Color color;
             TreeNode *p, *left, *right;
         };
         
@@ -29,6 +35,12 @@ namespace containers
         iterator find_impl(iterator node, KeyT key);
         iterator findMin_impl(iterator node);
         iterator findMax_impl(iterator node);
+
+        void leftRotate(iterator node);
+        void rightRotate(iterator node);
+
+        iterator insertFixup(iterator node);
+        
     };
 
     template <typename KeyT, typename Comp>
@@ -93,32 +105,113 @@ namespace containers
     }
 
     template <typename KeyT, typename Comp>
+    void SearchTree<KeyT, Comp>::leftRotate(iterator x) {
+        TreeNode *y = x->right;
+        x->right = y->left;
+        if (y->left)
+            y->left->p = x;
+        y->p = x->p;
+        if (x->p == nullptr)
+            root_ = y;
+        else if (x == x->p->left)
+            x->p->left = y;
+        else
+            x->p->right = y;
+        y->left = x;
+        x->p = y;
+    }
+
+    template <typename KeyT, typename Comp>
+    void SearchTree<KeyT, Comp>::rightRotate(iterator y) {
+        TreeNode *x = y->left;
+        y->left = x->right;
+        if (x->right)
+            x->right->p = y;
+        x->p = y->p;
+        if (y->p == nullptr)
+            root_ = x;
+        else if (y == y->p->left)
+            y->p->left = x;
+        else
+            y->p->right = x;
+        x->right = y;
+        y->p = x;
+    }
+
+    template <typename KeyT, typename Comp>
 	SearchTree<KeyT, Comp>::iterator SearchTree<KeyT, Comp>::insert(KeyT key) {
-        TreeNode *new_node = new TreeNode{key, nullptr, nullptr, nullptr};
+        TreeNode *z = new TreeNode{key, Color::Red, nullptr, nullptr, nullptr};
         TreeNode *x = root_, *y = nullptr;
 
         while (x) {
             y = x;
-            if (Comp(key, x->key))
+            if (Comp(z->key, x->key))
                 x = x->left;
             else
                 x = x->right;
         }
 
-        new_node->p = y;
+        z->p = y;
         if (!y)
-            root_ = new_node;
-        else if (Comp(key, y->key))
-            y->left = new_node;
+            root_ = z;
+        else if (Comp(z->key, y->key))
+            y->left = z;
         else
-            y->right = new_node;
+            y->right = z;
         
-        return new_node;
+        z = insertFixup(z);
+
+        return z;
+    }
+
+    template <typename KeyT, typename Comp>
+	SearchTree<KeyT, Comp>::iterator SearchTree<KeyT, Comp>::insertFixup(iterator z) {
+        while (z->p->color == Color::Red) {
+            if (z->p == z->p->p->left) {
+                TreeNode *y = z->p->p->right;
+                if (y->color == Color::Red) {
+                    z->p->color = Color::Black;
+                    y->color = Color::Black;
+                    z->p->p->color = Color::Red;
+                    z = z->p->p;
+                }
+                else {
+                    if (z == z->p->right) {
+                        z = z->p;
+                        leftRotate(z);
+                    }
+                    z->p->color = Color::Black;
+                    z->p->p->color = Color::Red;
+                    rightRotate(z->p->p);
+                }
+            }
+            else {
+                TreeNode *y = z->p->p->left;
+                if (y->color == Color::Red) {
+                    z->p->color = Color::Black;
+                    y->color = Color::Black;
+                    z->p->p->color = Color::Red;
+                    z = z->p->p;
+                }
+                else {
+                    if (z == z->p->left) {
+                        z = z->p;
+                        rightRotate(z);
+                    }
+                    z->p->color = Color::Black;
+                    z->p->p->color = Color::Red;
+                    leftRotate(z->p->p);
+                }
+            }
+        }
+        
+        root_->color = Color::Black;
+        return z;
     }
 
     template <typename KeyT, typename Comp>
 	void SearchTree<KeyT, Comp>::erase(iterator node) {
-        
+
     }
 
 }
